5cbb05671200eebe1b9b4f4e000b0f807b82bf71
Eli <gemaelisabet@gmail.com>
2016-03-22 22:30:45 -0300

    Actividad Degradé Color que no anda!

--- app/src/main/java/ar/edu/untref/procesamientoimagenes/actividad/ActividadDegrade.java
+++ app/src/main/java/ar/edu/untref/procesamientoimagenes/actividad/ActividadDegrade.java
@@ -72,31 +72,57 @@ public class ActividadDegrade extends ActividadBasica {
     private void generarDegradeColores() {
 
         int ancho = 256;
-        int alto = 256;
+        int alto = 1600;
 
         Bitmap bitmapDegradeGrises = Bitmap.createBitmap(ancho, alto, Bitmap.Config.RGB_565);
-
-        //TODO: Llenar toda la primer fila de 0, la segunda de 1, la tercera de 2, etc.
-        //Por ejemplo:
-        // bitmapDegradeGrises.setPixel(0, 0, Color.rgb(0,0,0));
-        // bitmapDegradeGrises.setPixel(0, 0, 0); <--- Es lo mismo que lo de arriba pero sirve solo para grises.
-        // Esa línea hace que el píxel en la posición 0, 0 sea de color negro.
         int i;
         int j;
+        int y=0;
 
         for(i=0;i<=255;i++){
             for(j=0;j<=255;j++){
-                bitmapDegradeGrises.setPixel(i, j, Color.rgb(i, i, i));
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( 255 , 0 , j ));
+                y=y+1;
+            }
+        }
+        for(i=0;i<=255;i++){
+            for(j=0;j<=255;j++){
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( 255-j , 0 , 255 ));
+                y=y+1;
+            }
+        }
+        for(i=0;i<=255;i++){
+            for (j = 0; j <= 255; j++) {
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( 0 , j , 255 ));
+                y=y+1;
+            }
+        }
+        for(i=0;i<=255;i++){
+            for (j = 0; j <= 255; j++) {
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( 0 ,  255 , 255-j ));
+                y=y+1;
+            }
+        }
+        for(i=0;i<=255;i++){
+            for (j = 0; j <= 255; j++) {
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( j , 255 , 0 ));
+                y=y+1;
             }
 
         }
+        for(i=0;i<=255;i++){
+            for (j = 0; j <= 255; j++) {
+                bitmapDegradeGrises.setPixel(i, y, Color.rgb( 255 , 255-j , 0 ));
+                y=y+1;
+            }
 
+        }
 
         //Muestro lo que dibujaste así ves cómo va quedando
 
         try {
             File archivo = getAplicacion().guardarArchivo(bitmapDegradeGrises, "/", "degradeGrises.png");
-            getAplicacion().mostrarImagen(archivo, degradeGrises);
+            getAplicacion().mostrarImagen(archivo, degradeColores);
         } catch (IOException e) {
             e.printStackTrace();
         }
