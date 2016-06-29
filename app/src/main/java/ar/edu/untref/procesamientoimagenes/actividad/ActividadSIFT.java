94636be2ca895c70b05b677b872f7a807f7d9257
maribelmai <maribel@develup.com.ar>
2016-06-29 16:14:30 -0300

    SIFT (Matcheo de puntos caracteristicos)

--- app/src/main/java/ar/edu/untref/procesamientoimagenes/actividad/ActividadSIFT.java
+++ app/src/main/java/ar/edu/untref/procesamientoimagenes/actividad/ActividadSIFT.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,15 +13,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDMatch;
 import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.features2d.DMatch;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.Features2d;
+import org.opencv.features2d.KeyPoint;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
 import ar.edu.untref.procesamientoimagenes.Aplicacion;
 import ar.edu.untref.procesamientoimagenes.R;
@@ -34,13 +45,16 @@ import butterknife.OnClick;
  */
 public class ActividadSIFT extends ActividadBasica {
 
-    private static final String LOG_TAG = ActividadOperaciones.class.getSimpleName();
+    private static final String LOG_TAG = ActividadSIFT.class.getSimpleName();
 
     @Bind(R.id.imagen1)
     ImageView imagen1;
 
     @Bind(R.id.imagen2)
     ImageView imagen2;
+
+    @Bind(R.id.resultado)
+    ImageView resultado;
 
     private File imagen;
 
@@ -125,6 +139,141 @@ public class ActividadSIFT extends ActividadBasica {
         imagen1.setImageBitmap(mutable);
     }
 
+    @OnClick(R.id.comparar)
+    public void comparar() {
+
+        Mat refMat = new Mat();
+        Mat srcMat = new Mat();
+
+        Bitmap refBitmap = ((BitmapDrawable) imagen1.getDrawable()).getBitmap();
+        refBitmap = refBitmap.copy(Bitmap.Config.RGB_565, true);
+        Bitmap srcBitmap = ((BitmapDrawable) imagen2.getDrawable()).getBitmap();
+        srcBitmap = srcBitmap.copy(Bitmap.Config.RGB_565, true);
+
+        if (refBitmap != null && srcBitmap != null) {
+
+            Utils.bitmapToMat(refBitmap, refMat);
+            Utils.bitmapToMat(srcBitmap, srcMat);
+
+            MatOfDMatch matches = new MatOfDMatch();
+            MatOfDMatch goodMatches = new MatOfDMatch();
+
+            LinkedList<DMatch> listOfGoodMatches = new LinkedList<>();
+
+            LinkedList<Point> refObjectList = new LinkedList<>();
+            LinkedList<Point> srcObjectList = new LinkedList<>();
+
+            MatOfKeyPoint refKeypoints = new MatOfKeyPoint();
+            MatOfKeyPoint srcKeyPoints = new MatOfKeyPoint();
+
+            Mat refDescriptors = new Mat();
+            Mat srcDescriptors = new Mat();
+
+            MatOfPoint2f reference = new MatOfPoint2f();
+            MatOfPoint2f source = new MatOfPoint2f();
+
+            FeatureDetector orbFeatureDetector = FeatureDetector.create(FeatureDetector.ORB);
+            orbFeatureDetector.detect(refMat, refKeypoints);
+            orbFeatureDetector.detect(srcMat, srcKeyPoints);
+
+            DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+            descriptorExtractor.compute(refMat, refKeypoints, refDescriptors);
+            descriptorExtractor.compute(srcMat, srcKeyPoints, srcDescriptors);
+
+            DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+            matcher.match(refDescriptors, srcDescriptors, matches);
+
+            double max_dist = 0;
+            double min_dist = 100;
+            List<DMatch> matchesList = matches.toList();
+
+            for (int i = 0; i < refDescriptors.rows(); i++) {
+                Double distance = (double) matchesList.get(i).distance;
+                if (distance < min_dist) min_dist = distance;
+                if (distance > max_dist) max_dist = distance;
+            }
+
+            for (int i = 0; i < refDescriptors.rows(); i++) {
+                if (matchesList.get(i).distance <= 1 * min_dist) {
+                    listOfGoodMatches.add(matchesList.get(i));
+                }
+            }
+
+            goodMatches.fromList(listOfGoodMatches);
+
+            List<KeyPoint> refObjectListKeypoints = refKeypoints.toList();
+            List<KeyPoint> srcObjectListKeypoints = srcKeyPoints.toList();
+
+            for (int i = 0; i < listOfGoodMatches.size(); i++) {
+                refObjectList.addLast(refObjectListKeypoints.get(listOfGoodMatches.get(i).queryIdx).pt);
+                srcObjectList.addLast(srcObjectListKeypoints.get(listOfGoodMatches.get(i).trainIdx).pt);
+            }
+
+            reference.fromList(refObjectList);
+            source.fromList(srcObjectList);
+
+//            String result;
+//            if (listOfGoodMatches.size() > 1 && listOfGoodMatches.size() < 200) {
+//                result = "They MATCH!";
+//            } else {
+//                result = "They DON'T match!";
+//            }
+//
+//            AlertDialog alert = new AlertDialog.Builder(this)
+//                    .setMessage(result)
+//                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            // close
+//                        }
+//                    }).create();
+//            alert.show();
+
+            Imgproc.cvtColor(refMat, refMat, Imgproc.COLOR_RGBA2RGB, 1);
+            Imgproc.cvtColor(srcMat, srcMat, Imgproc.COLOR_RGBA2RGB, 1);
+
+            Mat outputImage = new Mat();
+            Bitmap comboBmp = combineImages(refBitmap, srcBitmap);
+            Utils.bitmapToMat(comboBmp, outputImage);
+
+            Features2d.drawMatches(refMat, refKeypoints, srcMat, srcKeyPoints, goodMatches, outputImage);
+
+            Bitmap bitmap = Bitmap.createBitmap(outputImage.cols(), outputImage.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(outputImage, bitmap);
+            resultado.setImageBitmap(bitmap);
+//            imagen1.setImageBitmap(comboBmp);
+//            imagen1.invalidate();
+//            imagen2.setImageBitmap(bitmap);
+//            imagen2.invalidate();
+        }
+        else{
+            Toast.makeText(this, "Selecciona dos imágenes", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public Bitmap combineImages(Bitmap c, Bitmap s) {
+        Bitmap cs;
+
+        int width, height;
+
+        if(c.getWidth() > s.getWidth()) {
+            width = c.getWidth() + s.getWidth();
+            height = c.getHeight();
+        } else {
+            width = s.getWidth() + s.getWidth();
+            height = c.getHeight();
+        }
+
+        cs = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas comboImage = new Canvas(cs);
+
+        comboImage.drawBitmap(c, 0f, 0f, null);
+        comboImage.drawBitmap(s, c.getWidth(), 0f, null);
+
+        return cs;
+    }
+
     @Override
     public void finish() {
 
@@ -132,7 +281,7 @@ public class ActividadSIFT extends ActividadBasica {
         File file = null;
 
         try {
-            file = getAplicacion().guardarArchivo(((BitmapDrawable) imagen1.getDrawable()).getBitmap(), "/tmp/", nombreOriginal.substring(0, nombreOriginal.lastIndexOf(".")) + "_" + System.currentTimeMillis() + nombreOriginal.substring(nombreOriginal.lastIndexOf(".")));
+            file = getAplicacion().guardarArchivo(((BitmapDrawable) resultado.getDrawable()).getBitmap(), "/tmp/", nombreOriginal.substring(0, nombreOriginal.lastIndexOf(".")) + "_" + System.currentTimeMillis() + nombreOriginal.substring(nombreOriginal.lastIndexOf(".")));
         } catch (Exception e) {
             Log.e(LOG_TAG, "Temporal no se pudo guardar: " + e);
         }
